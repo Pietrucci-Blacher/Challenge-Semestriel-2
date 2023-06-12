b731fd0a875d660b2ac3722ceeef533f58de6ba6
Pietrucci-Blacher <maxime.pietrucci@gmail.com>
2023-06-12 19:17:11 +0200

    Add more stuff and tailwind and start login and register dev

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,22 +1,3 @@
-<script setup>
-import { ref, computed } from 'vue';
-import Home from '@/views/Home.vue';
-import NotFound from '@/views/errors/404.vue';
-
-const routes = {
-    '/': Home,
-};
-
-const currentPath = ref(window.location.pathname);
-window.addEventListener('popstate', () => {
-    currentPath.value = window.location.pathname;
-});
-
-const currentView = computed(() => {
-    return routes[currentPath.value.slice(1) || '/'] || NotFound;
-});
-</script>
-
 <template>
-    <component :is="currentView || NotFound" />
+    <router-view />
 </template>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,3 +1,10 @@
+<script setup>
+import Login from '@/components/Login.vue';
+import Register from '@/components/Register.vue';
+</script>
+
 <template>
     <h1>It's Me Mario</h1>
+    <Login />
+    <Register />
 </template>
