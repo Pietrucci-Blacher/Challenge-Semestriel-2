f49d5eb1a83278510c0c8fb4faf6615ea59f6b3c
Iliès <islim1@myges.fr>
2023-06-21 18:56:15 +0200

    modif modal + modal register

--- client/src/components/Modale.vue
+++ client/src/components/Modale.vue
@@ -97,44 +97,7 @@ import { ref } from 'vue';
 
 </script>
 
-<style scoped>
+<style >
 
-    .bloc-modale {
-        position: fixed;
-        top: 0;
-        left: 0;
-        bottom: 0;
-        right: 0;
-        width: 100%;
-        height: 100%;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-
-    .overlay {
-        position: fixed;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        background-color: rgba(0, 0, 0, .5);
-    }
-    
-    .modale {
-       background: #f1f1f1;
-       color: #333;
-       padding: 50px;
-       position: fixed;
-       top: 30%;
-    }
-    
-    .btn-modale {
-        position: absolute;
-        top: 10px;
-        right: 10px;
-        cursor: pointer;
-        background: white;
-    }
-
+  
 </style>
--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,37 +1,48 @@
 <template>
-    <div>
-        <h1>Register</h1>
-        <form @submit.prevent="submitForm">
-            <div>
-                <label for="username">Username:</label>
-                <input type="text" id="username" v-model="form.username" />
+        <div class="bloc-modale" v-if="showRegister">
+            <div class="overlay" v-on:click="toggleModaleRegister">
+                <div class="modale">
+                    <div v-on:click="toggleModaleRegister" class="btn-modale cursor-pointer" >&times;</div>
+                    <div>
+                        <h1>Register</h1>
+                        <form @submit.prevent="submitForm">
+                            <div>
+                                <label for="username">Username:</label>
+                                <input type="text" id="username" v-model="form.username" />
+                            </div>
+                            <div>
+                                <label for="email">Email:</label>
+                                <input type="email" id="email" v-model="form.email" />
+                            </div>
+                            <div>
+                                <label for="password">Password:</label>
+                                <input type="password" id="password" v-model="form.password" />
+                            </div>
+                            <div>
+                                <label for="confirmPassword">Confirm Password:</label>
+                                <input
+                                    type="password"
+                                    id="confirmPassword"
+                                    v-model="form.confirmPassword"
+                                />
+                            </div>
+                            <div v-if="error" class="error-message">{{ error }}</div>
+                            <div>
+                                <button type="submit">Register</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
-            <div>
-                <label for="email">Email:</label>
-                <input type="email" id="email" v-model="form.email" />
-            </div>
-            <div>
-                <label for="password">Password:</label>
-                <input type="password" id="password" v-model="form.password" />
-            </div>
-            <div>
-                <label for="confirmPassword">Confirm Password:</label>
-                <input
-                    type="password"
-                    id="confirmPassword"
-                    v-model="form.confirmPassword"
-                />
-            </div>
-            <div v-if="error" class="error-message">{{ error }}</div>
-            <div>
-                <button type="submit">Register</button>
-            </div>
-        </form>
-    </div>
+        </div>
 </template>
 
 <script>
 export default {
+
+    name: 'ModaleRegister',
+    props: ['showRegister', 'toggleModaleRegister'],
+
     data() {
         return {
             form: {
