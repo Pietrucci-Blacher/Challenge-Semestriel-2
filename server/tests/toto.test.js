93b182ff9e3f5a6990ddb251238dd308a314d79c
teddy <teddy.gauthier@outlook.com>
2023-06-22 15:28:44 +0200

    [feat] add begin user unit test

--- server/tests/toto.test.js
+++
@@ -1,8 +0,0 @@
-import { describe, it } from 'vitest';
-
-// The two tests marked with concurrent will be run in parallel
-describe('suite', () => {
-    it('serial test', async () => {
-        console.log('serial test');
-    });
-});
