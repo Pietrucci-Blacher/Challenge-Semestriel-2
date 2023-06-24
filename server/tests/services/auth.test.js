410776ce9730f5a974fc1d0f18a68d5cb390b6d2
teddy <teddy.gauthier@outlook.com>
2023-06-24 03:29:25 +0200

    [feat] add unit test for auth

--- server/tests/services/auth.test.js
+++ server/tests/services/auth.test.js
@@ -26,15 +26,10 @@ describe('Auth Service', () => {
                 where: { email },
             });
 
-            const comperedPassword = await bcrypt.compare(
-                password,
-                userInDb.password,
-            );
-
             expect(userInDb).toBeDefined();
             expect(userInDb.email).toEqual(email);
             expect(userInDb.username).toEqual(username);
-            expect(comperedPassword).toBeTruthy();
+            expect(bcrypt.compareSync(password, userInDb.password)).toBe(true);
         });
         it('should throw an error if user already exists', async () => {
             const email = faker.internet.email();
@@ -98,87 +93,6 @@ describe('Auth Service', () => {
             const result = AuthService.login(email, password);
 
             await expect(result).rejects.toThrow();
-        });
-    });
-
-    describe('refresh()', () => {
-        it('should refresh a token', async () => {
-            const password = faker.internet.password();
-            const email = faker.internet.email();
-            const username = faker.internet.userName();
-
-            const createdUser = await UserService.create({
-                username,
-                email,
-                password,
-            });
-
-            const accessToken = faker.string.alphanumeric(64);
-            const refreshToken = jwt.sign(
-                { id: createdUser.id },
-                JWT_REFRESH_SECRET,
-                { expiresIn: '1h' },
-            );
-
-            await TokenModel.create({
-                userId: createdUser.id,
-                refresh_token: refreshToken,
-                access_token: accessToken,
-                expire: new Date(),
-            });
-
-            const newAccessToken = await AuthService.refresh(refreshToken);
-
-            const decodedAccessToken = jwt.verify(
-                newAccessToken,
-                JWT_ACCESS_SECRET,
-            );
-
-            const tokenInDb = await TokenModel.findOne({
-                where: { userId: createdUser.id },
-            });
-
-            expect(newAccessToken).toBeDefined();
-            expect(decodedAccessToken.id).toEqual(createdUser.id);
-            expect(newAccessToken).not.toEqual(accessToken);
-            expect(newAccessToken).toEqual(tokenInDb.access_token);
-        });
-
-        it('should throw an error if token is invalid', async () => {
-            const refreshToken = faker.string.alphanumeric(64);
-
-            const result = AuthService.refresh(refreshToken);
-
-            await expect(result).rejects.toThrow();
-        });
-    });
-
-    describe('logout()', () => {
-        it('should logout a user', async () => {
-            const password = faker.internet.password();
-            const email = faker.internet.email();
-            const username = faker.internet.userName();
-
-            const createdUser = await UserService.create({
-                username,
-                email,
-                password,
-            });
-
-            await TokenModel.create({
-                userId: createdUser.id,
-                refresh_token: faker.string.alphanumeric(64),
-                access_token: faker.string.alphanumeric(64),
-                expire: new Date(),
-            });
-
-            await AuthService.logout(createdUser.id);
-
-            const tokenInDb = await TokenModel.findOne({
-                where: { userId: createdUser.id },
-            });
-
-            expect(tokenInDb).toBeNull();
         });
     });
 });
