113421ccdcca339cb6a3d2708d8674d578a9f222
Pietrucci-Blacher <maxime.pietrucci@gmail.com>
2023-07-01 16:41:35 +0200

    Add code for chat

---
+++ client/src/components/Chat.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="message in messages" :key="message.id">
+        {{ message.text }}
+      </li>
+    </ul>
+
+    <form @submit.prevent="sendMessage">
+      <input type="text" v-model="newMessage" placeholder="Type your message" />
+      <button type="submit">Send</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import { ref, onMounted, onBeforeUnmount } from 'vue';
+import io from 'socket.io-client';
+
+export default {
+  setup() {
+    const url = import.meta.env.VITE_VUE_APP_SOCKET_ENDPOINT;
+    const socket = io(url);
+
+    const messages = ref([]);
+    const newMessage = ref('');
+
+    const sendMessage = () => {
+      if (newMessage.value.trim() !== '') {
+        const message = {
+          id: Date.now(),
+          text: newMessage.value.trim(),
+        };
+        messages.value.push(message);
+        socket.emit('message', message);
+        newMessage.value = '';
+      }
+    };
+
+    onMounted(() => {
+      socket.on('message', (message) => {
+        messages.value.push(message);
+      });
+    });
+
+    onBeforeUnmount(() => {
+      socket.disconnect();
+    });
+
+    return {
+      messages,
+      newMessage,
+      sendMessage,
+    };
+  },
+};
+</script>
+
+<style scoped>
+/* Add custom styles for your chat component */
+</style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,10 +1,12 @@
 <script setup>
 import Login from '@/components/Login.vue';
 import Register from '@/components/Register.vue';
+import Chat from "@/components/Chat.vue";
 </script>
 
 <template>
     <Login />
     <br />
     <Register />
+    <Chat />
 </template>
