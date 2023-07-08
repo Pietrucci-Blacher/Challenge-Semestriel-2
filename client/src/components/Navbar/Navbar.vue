34c3831c20240270a7c2cbc0444735086f284b9c
Pietrucci-Blacher <maxime.pietrucci@gmail.com>
2023-07-08 09:10:49 +0200

    Feat: Add locale storage for theme

--- client/src/components/Navbar/Navbar.vue
+++ client/src/components/Navbar/Navbar.vue
@@ -1,7 +1,15 @@
 <template>
     <div
-        class="flex flex-col items-center justify-between h-full overflow-hidden text-gray-400"
-        :class="{ 'bg-gray-900': isDarkTheme, 'bg-gray-100': !isDarkTheme }"
+        :class="[
+            'flex',
+            'flex-col',
+            'items-center',
+            'justify-between',
+            'h-full',
+            'overflow-hidden',
+            'text-gray-400',
+            isDarkTheme ? 'bg-gray-900' : 'bg-gray-100',
+        ]"
     >
         <div>
             <router-link to="/">
@@ -16,10 +24,11 @@
                 />
             </router-link>
         </div>
-        <transition name="menu-toggle">
+        <transition name="menu-toggle" mode="out-in">
             <div
                 class="flex flex-col items-center border-t border-gray-700"
                 v-show="showMenu"
+                :key="showMenu"
             >
                 <router-link
                     v-for="item in menuItems"
@@ -27,39 +36,31 @@
                     :to="item.route"
                     class="flex items-center justify-center w-12 h-12 mt-2 rounded hover:bg-gray-700 hover:text-gray-300"
                 >
-                    <font-awesome-icon :icon="['fas', item.icon]" />
+                    <font-awesome-icon :icon="item.icon" />
+                    <span class="ml-2" v-show="showMenu">{{ item.name }}</span>
                 </router-link>
             </div>
         </transition>
-        <button class="transition-all" @click="toggleMenu">
-            <transition name="arrow-toggle">
-                <template v-slot:default>
-                    <font-awesome-icon
-                        :icon="
-                            showMenu
-                                ? ['fas', 'chevron-left']
-                                : ['fas', 'chevron-right']
-                        "
-                    />
-                </template>
-            </transition>
-        </button>
-        <button class="transition-all" @click="toggleTheme">
-            <transition name="theme-toggle">
-                <template v-slot:default>
-                    <font-awesome-icon
-                        :icon="isDarkTheme ? ['fas', 'sun'] : ['fas', 'moon']"
-                    />
-                    <span class="sr-only">
-                        {{
-                            isDarkTheme
-                                ? 'Switch to Light Theme'
-                                : 'Switch to Dark Theme'
-                        }}
-                    </span>
-                </template>
-            </transition>
-        </button>
+        <div class="flex flex-col items-center justify-evenly">
+            <button class="transition-all" @click="toggleTheme">
+                <transition name="theme-toggle">
+                    <template v-slot:default>
+                        <font-awesome-icon
+                            :icon="isDarkTheme ? 'sun' : 'moon'"
+                        />
+                    </template>
+                </transition>
+            </button>
+            <button class="transition-all" @click="toggleMenu">
+                <transition name="arrow-toggle">
+                    <template v-slot:default>
+                        <font-awesome-icon
+                            :icon="showMenu ? 'chevron-left' : 'chevron-right'"
+                        />
+                    </template>
+                </transition>
+            </button>
+        </div>
     </div>
 </template>
 
@@ -117,9 +118,26 @@ export default {
             );
         };
 
+        // Watch window resize event
+        watch(() => {
+            window.addEventListener('resize', toggleMenu);
+            return () => {
+                window.removeEventListener('resize', toggleMenu);
+            };
+        });
+
         // Restore theme mode and menu state from localStorage
-        isDarkTheme.value = localStorage.getItem('themeMode') === 'dark';
-        showMenu.value = localStorage.getItem('menuState') === 'open';
+        if (localStorage.getItem('themeMode') === 'dark') {
+            isDarkTheme.value = true;
+        } else {
+            isDarkTheme.value = false;
+        }
+
+        if (localStorage.getItem('menuState') === 'open') {
+            showMenu.value = true;
+        } else {
+            showMenu.value = false;
+        }
 
         return {
             isDarkTheme,
@@ -170,11 +188,13 @@ button {
 
 .menu-toggle-enter-active,
 .menu-toggle-leave-active {
-    transition: transform 0.3s ease;
+    transition: height 0.3s ease;
 }
 
 .menu-toggle-enter,
 .menu-toggle-leave-to {
-    transform: translateY(100%);
+    height: 0;
+    opacity: 0;
+    overflow: hidden;
 }
 </style>
