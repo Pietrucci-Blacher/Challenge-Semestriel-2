95b199ce1088c3b7407b732a25310d16c1639230
teddy <teddy.gauthier@outlook.com>
2023-07-09 14:55:46 +0200

    [feat] add mongo memory server

--- server/database/mongo/mongo.js
+++ server/database/mongo/mongo.js
@@ -1,12 +1,21 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import { MongoMemoryServer } from 'mongodb-memory-server';
 
 dotenv.config({ path: '../.env' });
 
-const { MONGO_USER, MONGO_PASSWORD, MONGO_DB } = process.env;
+const { MONGO_USER, MONGO_PASSWORD, MONGO_DB, MODE } = process.env;
+let mongo;
 
-const uri = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@127.0.0.1/${MONGO_DB}`;
-const mongo = await mongoose.connect(uri);
-console.log('Connected to mongoDB');
+if (MODE === 'test') {
+    const mongod = await MongoMemoryServer.create();
+    const uri = mongod.getUri();
+    mongo = await mongoose.connect(uri);
+    console.log('Connected to mongoDB memory server');
+} else {
+    const uri = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@127.0.0.1/${MONGO_DB}`;
+    mongo = await mongoose.connect(uri);
+    console.log('Connected to mongoDB');
+}
 
 export default mongo;
