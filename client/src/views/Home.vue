c10be65d448380cf560cd7c2e1c7cadb9c459f53
Pietrucci-Blacher <maxime.pietrucci@gmail.com>
2023-07-11 13:29:41 +0200

    Remove previous auth check

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -7,27 +7,6 @@ import Error from '@/views/Error.vue';
 import Chat from "@/components/Chat.vue";
 import Chat from '@/components/Chat.vue';
 import Navbar from '@/components/Navbar/Navbar.vue';
-
-import { ref, onMounted, defineExpose } from 'vue';
-
-const isUserAuthenticated = ref(false);
-
-const checkAuthentication = async () => {
-    try {
-        const response = await fetch('/api/check-authentication');
-        const data = await response.json();
-        data.isAuthenticated = undefined;
-        isUserAuthenticated.value = data.isAuthenticated;
-    } catch (error) {
-        console.error('Error checking authentication:', error);
-    }
-};
-
-onMounted(checkAuthentication);
-
-defineExpose({
-    isUserAuthenticated,
-});
 </script>
 
 <template>
