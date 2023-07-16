18d594a44284b47780d46ffdaf8961dbf61bad5b
Pietrucci-Blacher <maxime.pietrucci@gmail.com>
2023-07-16 08:28:43 +0200

    Import old files to new base

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -3,6 +3,26 @@ import Login from '@/components/Login.vue';
 import Register from '@/components/Register.vue';
 import Chat from '@/components/Chat.vue';
 import Navbar from '@/components/Navbar/Navbar.vue';
+
+const clientIdDiscord = import.meta.env.VITE_DISCORD_CLIENT_ID;
+const redirectUriDiscord = import.meta.env.VITE_DISCORD_REDIRECT_URI;
+const responseTypeDiscord = 'code';
+const scopeDiscord = 'identify email';
+const LoginUrlDiscord = `https://discord.com/api/oauth2/authorize?client_id=${clientIdDiscord}&redirect_uri=${encodeURIComponent(
+    redirectUriDiscord,
+)}&response_type=${responseTypeDiscord}&scope=${encodeURIComponent(
+    scopeDiscord,
+)}`;
+
+const clientIdGoogle = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+const redirectUriGoogle = import.meta.env.VITE_GOOGLE_REDIRECT_URI;
+const responseTypeGoogle = 'code';
+const scopeGoogle = 'openid email profile';
+const LoginUrlGoogle = `https://accounts.google.com/o/oauth2/v2/auth/userinfo.profile?client_id=${clientIdGoogle}&redirect_uri=${encodeURIComponent(
+    redirectUriGoogle,
+)}&response_type=${responseTypeGoogle}&scope=${encodeURIComponent(
+    scopeGoogle,
+)}`;
 </script>
 
 <template>
@@ -13,6 +33,9 @@ import Navbar from '@/components/Navbar/Navbar.vue';
             <br />
             <Register />
             <Chat />
+            <a :href="LoginUrlDiscord">Login with Discord</a>
+            <br />
+            <a :href="LoginUrlGoogle">Login with Google</a>
         </main>
     </section>
 </template>
