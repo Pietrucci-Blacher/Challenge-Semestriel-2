f3df7e9f68e635da389a3bb6e2804855c7ccdd1d
Pietrucci-Blacher <maxime.pietrucci@gmail.com>
2023-07-27 09:34:04 +0200

    Add Notification for register

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -28,64 +28,64 @@
             </div>
         </form>
     </div>
+    <Notification
+        v-if="registrationSuccessful"
+        type="success"
+        title="Registration successful"
+        message="You can now log in."
+    />
 </template>
 
-<script>
-export default {
-    data() {
-        return {
-            form: {
-                username: '',
-                email: '',
-                password: '',
-                confirmPassword: '',
+<script setup>
+import Notification from '@/components/Notification.vue';
+import { ref } from 'vue';
+
+const registrationSuccessful = ref(false);
+const form = ref({
+    username: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+});
+const error = ref('');
+
+async function submitForm() {
+    const { confirmPassword, ...requestData } = form.value;
+
+    if (requestData.password !== confirmPassword) {
+        error.value = 'Password and Confirm Password do not match.';
+        return;
+    }
+
+    try {
+        let url = import.meta.env.VITE_ENDPOINT_BACK_URL;
+        let endpoint = `${url}/auth/register`;
+        const response = await fetch(endpoint, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
             },
-            error: '',
-        };
-    },
-    methods: {
-        async submitForm() {
-            const { confirmPassword, ...requestData } = this.form;
+            body: JSON.stringify(requestData),
+        });
 
-            if (requestData.password !== confirmPassword) {
-                this.error = 'Password and Confirm Password do not match.';
-                return;
-            }
-
-            try {
-                let url = import.meta.env.VITE_ENDPOINT_BACK_URL;
-                let endpoint = `${url}/auth/register`;
-                const response = await fetch(endpoint, {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify(requestData),
-                });
-
-                if (response.ok) {
-                    // Registration successful, handle the response
-                    console.log('Registration successful');
-                    // Reset form fields after successful registration if needed
-                    this.form = {
-                        username: '',
-                        email: '',
-                        password: '',
-                        confirmPassword: '',
-                    };
-                } else {
-                    // Registration failed, handle the error
-                    const errorData = await response.json();
-                    this.error = errorData.message || 'Registration failed';
-                }
-            } catch (error) {
-                // Network error or API request failed
-                this.error = 'Registration failed';
-                console.error(error);
-            }
-        },
-    },
-};
+        if (response.ok) {
+            // Registration successful, handle the response
+            registrationSuccessful.value = true;
+            // Reset form fields after successful registration if needed
+            form.value.username = '';
+            form.value.email = '';
+            form.value.password = '';
+            form.value.confirmPassword = '';
+        } else {
+            const errorData = await response.json();
+            error.value = errorData.message || 'Registration failed';
+        }
+    } catch (error) {
+        // Network error or API request failed
+        error.value = 'Registration failed';
+        console.error(error);
+    }
+}
 </script>
 
 <style>
